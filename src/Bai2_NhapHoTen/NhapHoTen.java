c4a37b261ad28d2a7798353ef8c292f1cec0241d
theendboy21-cpu <theendboy21@gmail.com>
2026-09-18 22:27:11 +0700

    Hoan thanh bai 2

--- src/Bai2_NhapHoTen/NhapHoTen.java
+++ src/Bai2_NhapHoTen/NhapHoTen.java
@@ -11,6 +11,14 @@ import java.util.Scanner;
  * @author Lenovo
  */
 public class NhapHoTen {
-
-   
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap ho va ten: ");
+        String hoTen = sc.nextLine();
+        System.out.print("Nhap nam sinh: ");
+        int namSinh = sc.nextInt();
+        System.out.println("Ten: " + hoTen);
+        System.out.printf("Tuoi: %d", 2026 - namSinh);
+    }
 }
