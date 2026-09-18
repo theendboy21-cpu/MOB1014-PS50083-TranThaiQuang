a1b43e8cb845aef60a29bf2907fccd143fc52d03
theendboy21-cpu <theendboy21@gmail.com>
2026-09-18 21:58:47 +0700

    Hoan Thanh Bai 1

---
+++ src/Bai1_HelloJava/HelloJava.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Bai1_HelloJava;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.print("Ho ten: Tran Thai Quang; MSSV: PS500833");
+    }
+}
---
+++ src/Bai2_NhapHoTen/NhapHoTen.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Bai2_NhapHoTen;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class NhapHoTen {
+
+   
+}
