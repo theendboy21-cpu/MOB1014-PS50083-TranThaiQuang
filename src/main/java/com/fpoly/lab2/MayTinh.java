04cbb79837741799b91f8ddbb57f7fed4fea3949
theendboy21-cpu <theendboy21@gmail.com>
2026-09-25 23:13:33 +0700

    hoan thanh lab2

--- src/main/java/com/fpoly/lab2/MayTinh.java
+++ src/main/java/com/fpoly/lab2/MayTinh.java
@@ -4,10 +4,40 @@
  */
 package com.fpoly.lab2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Lenovo
  */
 public class MayTinh {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);    
+        double a,b,kq=0;
+        char op;
+        System.out.print("Nhap a:  ");
+        a = sc.nextDouble();
+        System.out.print("Nhap b:  ");
+        b = sc.nextDouble();
+        System.out.print("Nhap phep toan: ");
+        op = sc.next().charAt(0);
+        
+        switch (op){
+            case '+':
+                kq = a+b;
+                break;
+            case '-':
+                kq = a-b;
+                break;
+            case '*':
+                kq = a*b;
+                break;
+            case '/':
+                if(b==0) System.out.print("Khong the chia cho 0 \n");
+                else kq=a/b;
+                break;
+            default: System.out.print("Phep toan khong hop le\n");
+        }
+        System.out.printf("%.2f %c %.2f = %.2f",a,op,b,kq);
+    }
 }
--- src/main/java/com/fpoly/lab2/MuaTrongNam.java
+++ src/main/java/com/fpoly/lab2/MuaTrongNam.java
@@ -4,10 +4,38 @@
  */
 package com.fpoly.lab2;
 
+import java.util.Scanner;
+
+
 /**
  *
  * @author Lenovo
  */
 public class MuaTrongNam {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);    
+        int thang;
+        String mua = "";
+        System.out.print("Nhap thang: ");
+        thang = sc.nextInt();
+        
+        switch (thang){
+            case 1,2,3:
+                mua = "Mua xuan";
+                break;
+                case 4,5,6:
+                mua = "Mua ha";
+                break;
+                case 7,8,9:
+                mua = "Mua thu";
+                break;
+                case 10,11,12:
+                mua = "Mua dong";
+                break;
+                default: 
+                    System.out.println("Thang nhap khong hop le");
+                    return;
+        }
+        System.out.printf("Thang %d la mua &s",thang,mua);
+        }
 }
